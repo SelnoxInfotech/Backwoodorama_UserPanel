bf280c069eee533ed9032cf7cad335e503a588bf
Chandrajeet123 <selnoxreact@gmail.com>
2023-03-16 01:33:46 +0530

    uihiu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle'
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Components/Page/Login/Login"
 import { Routes, Route } from "react-router-dom"
+import Navbar from "./Components/Component/Navbar/Navbar"
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <Routes>
         <Route path="/" element={ <Login/> } />
         </Routes>
