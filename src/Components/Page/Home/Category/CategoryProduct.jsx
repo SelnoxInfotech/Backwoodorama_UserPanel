cf6d2e2c7323dff57ff99ea0bd08d766f2f125dc
Chandrajeet123 <selnoxreact@gmail.com>
2023-03-19 02:16:49 +0530

    dispens

--- src/Components/Page/Home/Category/CategoryProduct.jsx
+++ src/Components/Page/Home/Category/CategoryProduct.jsx
@@ -9,14 +9,14 @@ const CategoryProduct = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 8,
-        slidesToScroll: 1,
+        slidesToScroll: 4,
         // autoplay: true,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: false
                 }
@@ -26,14 +26,18 @@ const CategoryProduct = () => {
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 2,
-                    initialSlide: 2
+                    // initialSlide: 2,
+                    infinite: true,
+
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    infinite: true,
+
                 }
             }
         ]
--- src/Components/Page/Home/Dispensories/DispensoriesAddress.jsx
+++ src/Components/Page/Home/Dispensories/DispensoriesAddress.jsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {BiMap} from "react-icons/bi"
-import {AiOutlineStar} from "react-icons/ai"
+import { BiMap } from "react-icons/bi"
+import { AiOutlineStar } from "react-icons/ai"
 const DispensoriesAddress = () => {
     const settings = {
-   
+
         dots: true,
         infinite: true,
         speed: 500,
@@ -19,38 +19,54 @@ const DispensoriesAddress = () => {
                     slidesToShow: 2,
                     slidesToScroll: 1,
                     infinite: true,
-                    dots: false,
-               
+                    dots: true,
+
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                     slidesToScroll: 1,
-                    initialSlide: 1
+                    initialSlide: 1,
+                    dots: true,
+                    infinite: true,
+
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 599,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true,
+
                 }
             }
         ]
     }
-    const arr = [{ img_url: "./image/flower.png", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106",rating:"Rating" },
-     { img_url: "./image/flower2.webp", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106" ,rating:"Rating" },
-      { img_url: "./image/logo_png.png", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106",rating:"Rating"  },
-       { img_url: "./image/logo.webp", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106",rating:"Rating" },
-       { img_url: "./image/logo2.png", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106",rating:"Rating" },
-       { img_url: "./image/flower.png", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106" ,rating:"Rating" },
-       { img_url: "./image/logo.webp", address: "Canna Cabana",sec_add:"2917 Broadway astoria NY 11106",rating:"Rating" },
+    const arr = [{ img_url: "./image/flower.png", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
+    { img_url: "./image/flower2.webp", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
+    { img_url: "./image/logo_png.png", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
+    { img_url: "./image/logo.webp", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
+    { img_url: "./image/logo2.png", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
+    { img_url: "./image/flower.png", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
+    { img_url: "./image/logo.webp", address: "Canna Cabana", sec_add: "2917 Broadway astoria NY 11106", rating: "Rating" },
 
-    
 
-]
+
+    ]
 
     return (
         <>
@@ -61,34 +77,34 @@ const DispensoriesAddress = () => {
                 </div>
                 <div className='disp_wraper mt-4 '>
 
-                <Slider {...settings}>
+                    <Slider {...settings}>
 
-                {arr.map((ele,index)=>{
-                    return(
-                        <div className='disp_card_con' key={index}>
-                        <div className='dispen_card' >
-                        <div className='dis_center'>
-                        <div className='left_img_div'>
-                            <img src={ele.img_url} alt='img_not_found'/>
-                        </div>
-                        </div>
-                        <div className='dis_right_div'>
-                          <p>{ele.address}</p>
-                          <div className=' dis_navigation'>
-                         <span className='span_nav'><BiMap/></span> <h5>{ele.sec_add}</h5>
-                         </div>
-                        
-                             <div  className='dis_rating'>
-                            <h5>{ele.rating}</h5><span className='span_nav_star'><AiOutlineStar/></span>
-                            </div>
-                        </div>
+                        {arr.map((ele, index) => {
+                            return (
+                                <div className='disp_card_con' key={index}>
+                                    <div className='dispen_card' >
+                                        <div className='dis_center'>
+                                            <div className='left_img_div'>
+                                                <img src={ele.img_url} alt='img_not_found' />
+                                            </div>
+                                        </div>
+                                        <div className='dis_right_div'>
+                                            <p>{ele.address}</p>
+                                            <div className=' dis_navigation'>
+                                                <span className='span_nav'><BiMap /></span> <h5>{ele.sec_add}</h5>
+                                            </div>
 
-                    </div>
-                    </div>
-                    )
-                })}
-                   
-                </Slider>
+                                            <div className='dis_rating'>
+                                                <h5>{ele.rating}</h5><span className='span_nav_star'><AiOutlineStar /></span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            )
+                        })}
+
+                    </Slider>
                 </div>
 
             </div>
