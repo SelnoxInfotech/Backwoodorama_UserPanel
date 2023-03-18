9a176a5d1cd7f13d1cc14489b318d401f9cacf65
Selnoxinfotech <selnox93@gmail.com>
2023-03-18 17:59:43 +0530

    chnage

--- src/Components/Component/Map/StoreLocation.jsx
+++ src/Components/Component/Map/StoreLocation.jsx
@@ -1,36 +1,67 @@
-function initMap() {
-    const google = window.google;
-    // Create the map.
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: {lat: 52.632469, lng: -1.689423},
-    });
-  
-    // Load the stores GeoJSON onto the map.
-    map.data.loadGeoJson('stores.json', {idPropertyName: 'storeid'});
-  
-    const apiKey = 'AIzaSyBRchIzUTBZskwvoli9S0YxLdmklTcOicU';
-    const infoWindow = new google.maps.InfoWindow();
-  
-    // Show the information for a store when its marker is clicked.
-    map.data.addListener('click', (event) => {
-      const category = event.feature.getProperty('category');
-      const name = event.feature.getProperty('name');
-      const description = event.feature.getProperty('description');
-      const hours = event.feature.getProperty('hours');
-      const phone = event.feature.getProperty('phone');
-      const position = event.feature.getGeometry().get();
-      const content = `
-        <h2>${name}</h2><p>${description}</p>
-        <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
-      `;
-  
-      infoWindow.setContent(content);
-      infoWindow.setPosition(position);
-      infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
-      infoWindow.open(map);
-    });
-  }
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+export default function MapContainer () {
 
+<>
+<script>
+      var  initMap = function(){}
+      </script>
+    
+      <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRchIzUTBZskwvoli9S0YxLdmklTcOicU&callback=initMap">
+      </script>
 
-  export default initMap;
+</>
+  const mapStyles = {        
+    height: "100vh",
+    width: "100%"};
+  
+  const locations = [
+    {
+      name: "Location 1",
+      location: { 
+        lat: 23.259933,
+        lng: 77.412613
+      },
+    },
+    {
+      name: "Location 2",
+      location: { 
+        lat: 41.3917,
+        lng: 2.1649
+      },
+    },
+    {
+      name: "Location 3",
+      location: { 
+        lat: 41.3773,
+        lng: 2.1585
+      },
+    },
+    {
+      name: "Location 4",
+      location: { 
+        lat: 41.3797,
+        lng: 2.1682
+      },
+    },
+    {
+      name: "Location 5",
+      location: { 
+        lat: 41.4055,
+        lng: 2.1915
+      },
+    }
+  ];
+  
+  return (
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          >
+            <Marker  position={locations.location1}/>
+       
+     </GoogleMap>
+  )
+    
+      
+}
