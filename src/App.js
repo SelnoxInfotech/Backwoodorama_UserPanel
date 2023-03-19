a75488bbd5c6abb1131272d9c9c8ab62e5316195
swapnil9340 <95558311+swapnil9340@users.noreply.github.com>
2023-03-19 20:08:12 +0530

    v

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/login" element={ <Login/> } />
         <Route path='/CategoryProduct' element={<CategoryProduct/>}/>
          <Route  path="/DispensoriesAddress" element={<DispensoriesAddress/>}/>
-         <Route  path="/StoreLocationMap" element={<StoreLocationMap/>}/>
+        <Route path="/StoreLocationMap" element={<StoreLocationMap isMarkerShown />}/>
         </Routes>
     
     </div>
--- src/Components/Component/Map/StoreLocation.jsx
+++ src/Components/Component/Map/StoreLocation.jsx
@@ -1,67 +1,37 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-export default function MapContainer () {
+import React from "react";
+import ReactDOM from "react-dom";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
-<>
-<script>
-      var  initMap = function(){}
-      </script>
-    
-      <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRchIzUTBZskwvoli9S0YxLdmklTcOicU&callback=initMap">
-      </script>
+const MyMapComponent = compose(
+  withProps({
+    /**
+     * Note: create and replace your own key in the Google console.
+     * https://console.developers.google.com/apis/dashboard
+     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
+     */
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBRchIzUTBZskwvoli9S0YxLdmklTcOicU&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `800px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={10} defaultCenter={{ lat: 23.259933, lng: 77.412613 }}>
+    {props.isMarkerShown && (
+      <Marker position={{ lat: 22.7195687, lng: 75.8577258 }} />,
+      <Marker position={{ lat: 23.259933, lng: 77.412613 }} />
 
-</>
-  const mapStyles = {        
-    height: "100vh",
-    width: "100%"};
-  
-  const locations = [
-    {
-      name: "Location 1",
-      location: { 
-        lat: 23.259933,
-        lng: 77.412613
-      },
-    },
-    {
-      name: "Location 2",
-      location: { 
-        lat: 41.3917,
-        lng: 2.1649
-      },
-    },
-    {
-      name: "Location 3",
-      location: { 
-        lat: 41.3773,
-        lng: 2.1585
-      },
-    },
-    {
-      name: "Location 4",
-      location: { 
-        lat: 41.3797,
-        lng: 2.1682
-      },
-    },
-    {
-      name: "Location 5",
-      location: { 
-        lat: 41.4055,
-        lng: 2.1915
-      },
-    }
-  ];
-  
-  return (
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={13}
-          >
-            <Marker  position={locations.location1}/>
-       
-     </GoogleMap>
-  )
-    
-      
-}
+    )}
+  </GoogleMap>
+));
+
+export default MyMapComponent; 
+
