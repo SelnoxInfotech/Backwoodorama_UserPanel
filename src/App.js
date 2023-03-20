89beac411ac0c724e1465e227c45c75fdf70ca6d
Selnoxinfotech <selnox93@gmail.com>
2023-03-20 17:03:48 +0530

    chnage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import CategoryProduct from "./Components/Page/Home/Category/CategoryProduct";
 import DispensoriesAddress from "./Components/Page/Home/Dispensories/DispensoriesAddress"
 import StoreLocationMap from "./Components/Component/Map/StoreLocation"
 import WeedProduct from './Components/Page/Home/Weed/WeedProduct';
+import Map from './Components/Component/Map/map';
 function App() {
   return (
     <div>
@@ -20,9 +21,10 @@ function App() {
         <Route path='/CategoryProduct' element={<CategoryProduct/>}/>
          <Route  path="/DispensoriesAddress" element={<DispensoriesAddress/>}/>
          <Route  path="/StoreLocationMap" element={<StoreLocationMap/>}/>
+         <Route  path="/LocationMap" element={<Map/>}/>
          <Route  path="/WeedProduct" element={<WeedProduct/>}/>
 
-        <Route path="/StoreLocationMap" element={<StoreLocationMap isMarkerShown />}/>
+        <Route path="/StoreLocationMap" element={<StoreLocationMap  />}/>
         </Routes>
     
     </div>
