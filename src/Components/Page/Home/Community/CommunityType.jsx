5da0aff70829b4914aac0750825e4ba417156e01
Chandrajeet123 <selnoxreact@gmail.com>
2023-03-23 01:11:23 +0530

    fefd

--- src/Components/Page/Home/Community/CommunityType.jsx
+++ src/Components/Page/Home/Community/CommunityType.jsx
@@ -2,19 +2,20 @@ const CommunityType = () => {
     return (
         <>
             <div className="container-fluid mb-4">
-                <div className="row">
+                <div className="row" style={{ background: "#E8FFF1" }}>
+
                     <div className="col-12 d-flex" style={{ background: "#E8FFF1" }}>
 
                         <div className="col-6" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                             <div className="col-12 d-flex">
 
-                                <div className="col-6 mt-4  ">
-                                    <div className="col-12 community_img_div d-flex" >
-                                        <img src="./image/community_1.png" alt='img_not_found' />
-                                        <img src="./image/community_2.png" alt='img_not_found' />
+                                <div className="col-6 mt-4">
+                                    <div className="col-12  d-flex" >
+                                        <img src="./image/community_1.png" alt='img_not_found' width={"100%"} height={"400px"}/>
+                                        <img src="./image/community_2.png" alt='img_not_found' width={"100%"} height={"400px"}/>
 
                                     </div>
-                                   
+
                                 </div>
 
                             </div>
@@ -23,7 +24,7 @@ const CommunityType = () => {
                         <div className="col-6" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                             <div className="col-12 d-block">
 
-                                <div className="col-12  mt-4 comm_para">
+                                <div className="col-12  mt-4 comm_para1">
                                     <p>BACKWOODAROMA</p>
                                 </div>
                                 <div className="col-12 comm_para">
@@ -50,6 +51,42 @@ const CommunityType = () => {
 
                     </div>
 
+
+
+
+                    {/* <div className="col-6">
+                        <div className="row">
+                            <div className="col-6">
+                                <img src="./image/community_1.png" alt='img_not_found' width={"100%"} height={"400px"} />
+                            </div>
+                            <div className="col-6">
+                                <img src="./image/community_2.png" alt='img_not_found' width={"100%"} height={"400px"} />
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="col-6 com">
+                        <div className="row mt-4">
+                            <div className="col-12">
+                                <p>BACKWOODAROMA</p>
+                                <p>A community connecting canabis consumer retailers doctor and brands since 2008</p>
+                            </div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col-6 text-end">
+                                <p>Google Play</p>
+                            </div>
+                            <div className="col-6">
+                                <p>App Store</p>
+                            </div>
+
+                        </div>
+                        <div>
+
+                        </div>
+                    </div> */}
 
                 </div>
             </div>
