bc61a32b4da04743bd71fd65b20b1ba5c97d5283
Selnoxinfotech <selnox93@gmail.com>
2023-04-03 10:23:52 +0530

    dfsf

--- src/Components/Page/Product/Component/ProductList.jsx
+++ src/Components/Page/Product/Component/ProductList.jsx
@@ -5,45 +5,115 @@ import Box from '@mui/material/Box';
 import { AiFillStar } from "react-icons/ai";
 import useStyles from "../../../../Style"
 import { Link } from "react-router-dom";
-import { useForm } from 'react-hook-form';
-import { Add } from "@mui/icons-material";
+import _ from "lodash";
+const ProductList = ({ arr }) => {
 
-const ProductList = ({ arr, btn }) => {
-    const [ProductImage, SetProductImage] = React.useState('')
-     const method = useForm()
+    const [Price, SetPrice] = React.useState([])
+
 
     const Addtocard = (Event) => {
-        const element = document.getElementById(Event);
-        console.log(element)
+        // const johnArr = _.filter(Price, Price => Price.Event.id === Event.id - 1)
+        // console.log(johnArr);
+        // SetPrice(Price.map((price)=>{
+
+
+        // }))
+
+
     }
+
+
+
+    const PriceSelect = (Product, Item) => {
+        //          Price.forEach((item) =>{ 
+        // console.log(Product)
+        //        item.Product_id === Product_id 
+        //        ?  SetPrice({...Price, Item_id: Item_id })
+        //        : SetPrice([...Price, { Product_id, Item_id }])
+        //    })
+
+        if (Price.length === 0) {
+            SetPrice([...Price, { Product_id: Product, Item_id: Item }])
+        }
+
+        if (Price.map((friend) => { return friend.Product_id === Product })) {
+            console.log(Price)
+           
+             SetPrice(
+                Price.map((friend) =>
+                    friend.Product_id === Product
+                        ? { ...friend, Item_id: Item }
+                        : { ...friend }
+                )
+            )
+        }
+        else  {
+
+            SetPrice([...Price, { Product_id: Product, Item_id: Item }])
+            // SetPrice(
+            //     Price.map((friend) =>
+            //         friend.Product_id === Product
+            //             ? { ...friend, Item_id: Item }
+            //             : { ...friend }
+            //     )
+            // )
+              console.log(Price)`   `
+        }
+        
+            //   
+        
+
+        // SetPrice((prev)=>prev.map((task)=>{
+        //     if(task.Product_id === Product_id){
+        //       return { ...Price, Item_id: Item_id }
+        //     }  
+        //     else{
+        //       return [...Price, { Product_id, Item_id }];
+        //     }
+        // }))
+
+        // console.log(Price)
+        // // SetPrice(
+        //     Price?.map((data) => { 
+        //     console.log(data.Product_id)
+        //     if (data.Product_id === Product_id) {
+        //         return SetPrice({ ...Price, Item_id: Item_id })
+        //     }
+        //     // else {
+        //     return( [...Price, { Product_id, Item_id }])
+        // }
+
+        //   return  data.Product_id === Product_id ? { ...Price, Item_id: Item_id } : [...Price, { Product_id, Item_id }]
+        // })   
+        // )
+        //  }
+
+    }
+    // console.log(Price)
 
     const classes = useStyles()
     return (
         <div className="col-12  prod_cat_display">
             {arr.map((ele, index) => {
-                   
-               
-                
                 return (
                     <div className="col-3 prod_inner_cont" key={index}>
-                      <form onSubmit ={method.handleSubmit(Addtocard)} >
-                      <div className="col-12 prod_main_cont  p-2">
+
+                        <div className="col-12 prod_main_cont  p-2">
                             <Link to="/ProductDetail" >
                                 <div className="col-4 prod_cat_cont">
                                     <div className="col-12 p-2 prod_cat_img">
                                         <img id={ele.id} src={`http://52.3.255.128:8000/${ele.images[0]?.image}`} alt="img_not_found" style={{ pointerEvents: "none" }} />
-                                    
+
                                         <div className="col prod_img_btn prodCat_gap d-flex">
                                             <button className="mx-2 cat_prod_inner_btn btn2">THC 70%</button>
                                         </div>
-                                        <button  className="cat_prod_inner_btn btn1">Indica</button>
+                                        <button className="cat_prod_inner_btn btn1">Indica</button>
 
                                     </div>
                                 </div>
                             </Link>
                             <div className="col-8 product_cat_allProduct">
                                 <div className="col-12 px-2 prod_para " style={{ marginBottom: "-10px" }}>
-                                    <p className="comm_head_prop fontStyle">{ele.address}</p>
                                 </div>
                                 <div className="col-12 px-2 prod_para" style={{ marginBottom: "-10px" }}>
                                     <p className='fontStyle common_sub_head'>{ele.Product_Name}</p>
@@ -52,31 +122,36 @@ const ProductList = ({ arr, btn }) => {
                                     <p className='fontStyle common_sub_head'>Rating</p><span className='span_nav_star'><AiFillStar className={classes.disPen_Icons} /></span>
                                 </div>
                                 <div className="col-12   prod_cat_cont_btn px-2">
-                                    {btn.map((ele, index) => {
+                                    {ele.Prices?.map((ele1, index) => {
+                                        var JsonObject = JSON.parse(JSON.stringify(ele1))
+                                        var jsondata = JSON.parse(JsonObject.Price)
                                         return (
-                                            <div className="col-3 prod_cat_btn_cont mt-2 d-flex" key={index} >
-                                                <button className="prod_cat_btns" value={ele.rs} >
-                                           {ele.quant} 
-                                              <p  className="rs">   {ele.rs}$</p>
-                                                 </button>
-                                            </div>
+                                            jsondata.map((data, index) => {
+                                                return (
+                                                    <div className="col-3 prod_cat_btn_cont mt-2 d-flex" key={index} >
+                                                        <section className="prod_cat_btns " id="active" value={data.id} onClick={() => PriceSelect(ele.id, data.id, data.id)} >
+                                                            {data.Weight}
+                                                            <p className="rs">{data.Price}$</p>
+                                                        </section>
+                                                    </div>
+                                                )
+                                            })
                                         )
                                     })}
                                 </div>
                                 <div className="col-12 d-flex mt-3 mb-2">
-                          
-                                <MdOutlineShoppingCart  className={classes.muiIcons} />
-                               
+
+                                    <MdOutlineShoppingCart onClick={() => { Addtocard(ele) }} className={classes.muiIcons} />
+
                                     <Box
                                         className={` weed_cart_btn ${classes.loadingBtnTextAndBack}`}
                                         style={{ width: "83%" }}
                                     >
-                                        <LoadingButton onClick={() => Addtocard(ele.id)} variant="outlined">Buy Now</LoadingButton>
+                                        <LoadingButton variant="outlined">Buy Now</LoadingButton>
                                     </Box>
                                 </div>
                             </div>
-                        </div> 
-                      </form>
+                        </div>
                     </div>
                 )
             })}
