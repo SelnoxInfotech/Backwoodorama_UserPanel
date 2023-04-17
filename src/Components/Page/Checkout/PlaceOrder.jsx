aca2273cd856a29a6fbb112a2af63aac2369dda2
Chandrajeet123 <selnoxreact@gmail.com>
2023-04-17 16:54:33 +0530

    placeorder

--- src/Components/Page/Checkout/PlaceOrder.jsx
+++ src/Components/Page/Checkout/PlaceOrder.jsx
@@ -6,24 +6,46 @@ const PlaceOrder = () => {
     return (
         <>
             <div className="container-fluid">
-                <div className="row center">
-                    <div className="col-8 ThanYouOrder_Container_height">
-                        <div className="row top_container">
-                            <div className="col-12 ThankYouOrder_paragraph">
-                                <p>Thank you Maxwell</p>
+                <div className="row center p-2">
+                    <div className="col-12 col-lg-8 col-md-10 col-sm-10 ThanYouOrder_Container_height">
+                        <div className="row p-2">
+                            <div className='col-12 top_container '>
+                                <div className='row'>
+                                    <div className="col-12 ThankYouOrder_paragraph ">
+                                        <p>Thank you Maxwell</p>
+
+                                    </div>
+                                    <div className="col-12">
+                                        <p>You'll receive a confirmation email soon</p>
+
+                                    </div>
+                                    <div className="col-12">
+                                        <p>Order number:#122</p>
+                                    </div>
+                                </div>
 
                             </div>
-                            <div className="col-12">
-                                <p>You'll receive a confirmation email soon</p>
 
-                            </div>
-                            <div className="col-12">
-                                <p>Order number:#122</p>
+
+                        </div>
+                        <div className='row'>
+                            <div className='col-12 important_message_div_height'>
+                                <div className='row'>
+                                    <div className='col-12'>
+                                        <p>Important message for your order</p>
+                                    </div>
+                                    <div className='col-12'>
+                                        <p>Due to social distancing guidlines only the patient in
+                                            the dispansary</p>
+                                    </div>
+
+                                </div>
+
                             </div>
 
                         </div>
-                        <div className="row  center">
-                            <div className="col-10 stepper_container mt-4">
+                        <div className="row  center p-2">
+                            <div className="col-12 stepper_container mt-4">
                                 <Stepper activeStep={1}>
                                     <Step className={classes.stepperBtn} label="Order place" />
                                     <Step label="Confirming your order" />
@@ -45,11 +67,11 @@ const PlaceOrder = () => {
 
                                 </div>
                                 <div className='row'>
-                                    <div className='col-2 order_detail_paragraph fontStyle'>
-                                      <p>Sub total</p>
+                                    <div className='col-6 col-lg-2 col-md-2 col-sm-6 order_detail_paragraph fontStyle'>
+                                        <p>Sub total</p>
                                     </div>
-                                    <div className='col-2 order_detail_paragraph fontStyle'>
-                                     <p>$400</p>
+                                    <div className='col-6 col-lg-2 col-md-2 col-sm-6 order_detail_paragraph fontStyle'>
+                                        <p>$400</p>
                                     </div>
 
                                 </div>
--- src/Components/Page/Home/Dashboard/ComponentDashboard/Banner/HomePageBanner.jsx
+++ src/Components/Page/Home/Dashboard/ComponentDashboard/Banner/HomePageBanner.jsx
@@ -7,8 +7,14 @@ const HomePageBanner = () => {
                     <div className="col-lg-12 HomePageBanner_height ">
                         <div className="row">
                             <div className="col-12 homePage_header fontStyle">
-                                <h1>DIRECTORY WEBSITE</h1>
+                                <h1>FIND YOUR STRAIN</h1>
 
+                            </div>
+                            <div className="col-12">
+                               <p>With thousands to choose from,find the </p>
+                            </div>
+                            <div className="col-12">
+                               <p>strain that fits your need </p>
                             </div>
                         </div>
 
