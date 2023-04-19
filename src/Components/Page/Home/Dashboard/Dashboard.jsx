7f1d12630b44167b58fe5aee558b3b6d02626525
Selnoxinfotech <selnox93@gmail.com>
2023-04-19 11:21:06 +0530

    FolderStr

--- src/Components/Page/Home/Dashboard/Dashboard.jsx
+++ src/Components/Page/Home/Dashboard/Dashboard.jsx
@@ -1,11 +1,11 @@
 import Map from "../../../Component/Map/map"
-import CategoryProduct from "./ComponentDashboard/Category/CategoryProduct";
-import DispensoriesAddress from "./ComponentDashboard/Dispensories/DispensoriesAddress";
-import LatestServices from "./ComponentDashboard/LatestArticle/LatestServices";
-import WeedProduct from "./ComponentDashboard/Weed/WeedProduct";
-import CommunityType from "./ComponentDashboard/Community/CommunityType";
+import CategoryProduct from "./ComponentDashboard/CategoryProduct";
+import DispensoriesAddress from "../../Dispansires/DispansiresComponent/DispensoriesAddress";
+import LatestServices from "./ComponentDashboard/LatestServices";
+import WeedProduct from "./ComponentDashboard/WeedProduct";
+import CommunityType from "./ComponentDashboard/CommunityType";
 import Footer from "../../../Component/Footer/Footer";
-import HomePageBanner from "./ComponentDashboard/Banner/HomePageBanner"
+import HomePageBanner from "./ComponentDashboard/HomePageBanner"
 // import Flavour from "../../Delivery/Flavour/Flavour";
 import light  from "../../../Component/Map/MapStyle"
 export default function Dashboard() {
