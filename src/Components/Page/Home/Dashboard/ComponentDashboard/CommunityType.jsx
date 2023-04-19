f931d9fd67162c8a2b636db72a8acf47d6f21964
Selnoxinfotech <selnox93@gmail.com>
2023-04-19 16:55:16 +0530

    Foterrre

--- src/Components/Page/Home/Dashboard/ComponentDashboard/CommunityType.jsx
+++ src/Components/Page/Home/Dashboard/ComponentDashboard/CommunityType.jsx
@@ -2,24 +2,21 @@ import { Link } from "react-router-dom"
 const CommunityType = () => {
     return (
         <>
-            {/* <div className="container-fluid mb-4 mt-4">
+            <div className="container-fluid mb-4 mt-4">
                 <div className="row">
 
-                    <div className="col-12 d-flex" style={{ background: "#E8FFF1" }}>
+                    <div className="col-12 d-flex" style={{ background: "#E8FFF1" ,height:"350px" }}>
 
-                        <div className="col-6" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                            <div className="col-12 d-flex">
+                        <div className="col-6 Footer_head_image"  style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
 
-                                <div className="col-6 mt-4">
-                                    <div className="col-12  d-flex">
-                                        <img src="./image/community_1.png" alt='img_not_found' width={"100%"} height={"400px"}/>
-                                        <img src="./image/community_2.png" alt='img_not_found' width={"100%"} height={"400px"}/>
+                           <div className="col-xs-2 col-md-4 col-xl-4">
+                           <img src="image/yt.png" alt='img_not_found' className="Footer_image"   />
+                            
+                           </div>
+                           <div className="col-xs-2  col-md-4 col-xl-4">
+                           <img src="image/yt.png" alt='img_not_found'  className="Footer_image"/>
 
-                                    </div>
-
-                                </div>
-
-                            </div>
+                           </div>
 
                         </div>
                         <div className="col-6" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -27,7 +24,7 @@ const CommunityType = () => {
 
                                 <div className="col-12  mt-4 ">
                                     <div className="col-10 comm_para1">
-                                    <p>BACKWOODAROMA</p>
+                                        <p>BACKWOODAROMA</p>
 
                                     </div>
                                 </div>
@@ -35,13 +32,13 @@ const CommunityType = () => {
                                     <p>A community connecting canabis consumer retailers doctor and brands since 2008 </p>
 
                                 </div>
-                                <div className="col-12 d-flex" style={{ gap: "10px" }}>
+                                <div className="col-12  Social"  style={{ gap: "10px" }}>
                                     <div className='col-6 text-end'>
-                                    <Link  to="#" ><img src= "./image/play_store.png" alt="img_not_found" width={"100px"} height={"30px"}></img> </Link>
+                                        <Link to="#" ><img src="./image/play_store.png" alt="img_not_found" width={"100px"} height={"30px"}></img> </Link>
 
                                     </div>
                                     <div className='col-6'>
-                                    <Link to="#"><img src="./image/apple_img.jpg" alt="img_not_found" width={"100px"} height={"30px"} style={{pointerEvents: "none"}}></img></Link>
+                                        <Link to="#"><img src="./image/apple_img.jpg" alt="img_not_found" width={"100px"} height={"30px"} style={{ pointerEvents: "none" }}></img></Link>
 
                                     </div>
 
@@ -55,7 +52,7 @@ const CommunityType = () => {
 
                     </div>
                 </div>
-            </div> */}
+            </div>
         </>
     )
 }
