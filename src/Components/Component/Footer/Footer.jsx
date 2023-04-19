1f8df5e6a60a59b54dc4c591c68898dda993c4ad
Chandrajeet123 <selnoxreact@gmail.com>
2023-04-19 16:54:37 +0530

    footer

--- src/Components/Component/Footer/Footer.jsx
+++ src/Components/Component/Footer/Footer.jsx
@@ -3,12 +3,13 @@ import { FaFacebook } from "react-icons/fa"
 import { FaInstagram } from "react-icons/fa"
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import useStyles from "../../../Style"
+import { Link } from "react-router-dom"
 const Footer = () => {
-    const classes=useStyles()
-    const AboutUs = [{ head1: "About us" }, { head: "company" }, { head: "Investor" }, { head: "Help Center" }, { head: "Download App" }]
-    const Category = [{ head2: "Category" }, { head: "Flower" }, { head: "CBD" }, { head: "Concentrate" }, { head: "Edible" }]
-    const Legal = [{ head3: "Legal" }, { head: "Term 7 conditions" }, { head: "Carrier" }, { head: "Privacy Policy" }]
-    const More = [{ head4: "More" }, { head: "Get started" }, { head: "Brand" }, { head: "Add business" }, { head: "Contact us" }]
+    const classes = useStyles()
+    const AboutUs = [ { head: "company" }, { head: "Investor" }, { head: "Help Center" }, { head: "Download App" }]
+    const Category = [{ head: "Flower" }, { head: "CBD" }, { head: "Concentrate" }, { head: "Edible" }]
+    const Legal = [{ head: "Term 7 conditions" }, { head: "Carrier" }, { head: "Privacy Policy" }]
+    const More = [{ head: "Get started" }, { head: "Brand" }, { head: "Add business" }, { head: "Contact us" }]
 
 
 
@@ -17,9 +18,9 @@ const Footer = () => {
             <div className="container-fluid">
                 <div className="row footer_Main_row">
                     <div className="col-12 footer_main_div_display">
-                        <div className="col-4 footer_logo_container px-4">
+                        <div className="col-4 footer_logo_container">
                             <div className="col-12 footer_logo footer_logo_height">
-                                <LazyLoadImage className="footer_logo_image" src="/image/logo.webp" alt="image_not found"/>
+                                <LazyLoadImage className="footer_logo_image" src="/image/logo.webp" alt="image_not found" />
 
                             </div>
                             <div className="col-12 footer_logo footer_log_heading_height">
@@ -27,10 +28,10 @@ const Footer = () => {
                             </div>
                             <div className="col-4  footer_logo_icons_height">
                                 <div className="col-2">
-                                    <BsWhatsapp className={`footer_icons ${classes.footer_icons_color}`}/>
+                                    <BsWhatsapp className={`footer_icons ${classes.footer_icons_color}`} />
                                 </div>
                                 <div className="col-2 ">
-                                    <FaFacebook className={` footer_icons ${classes.footer_icons_color}`}/>
+                                    <FaFacebook className={` footer_icons ${classes.footer_icons_color}`} />
                                 </div>
                                 <div className="col-2">
                                     <FaInstagram className={` footer_icons ${classes.footer_icons_color}`} />
@@ -41,40 +42,34 @@ const Footer = () => {
                             <div className="col-12  footer_inner_flex">
                                 <div className="col-6 footer_content_width_small">
                                     <div className="col-12 footer_inner_container">
-                                        <div className="col-6">
-                                            {AboutUs.map((ele, index) => {
-                                                return (
-                                                    <>
-                                                        <div className="col-12 fontStyle footer_heading footer_font_size">
-                                                            <p>{ele.head1}</p>
+                                        <div className="col-6 footer_list">
+                                            <ol className="footer_list_gap">
+                                                <h5 className="fontStyle">About us</h5>
+                                                {AboutUs.map((ele, index) => {
+                                                    return (
+                                                        <div key="index">
+                                                
+                                                <Link to="/"><li>{ele.head}</li></Link>
+                                                </div>
 
-                                                        </div>
-                                                        <div className="col-12 Footer_fontStyle footer_font_size footer_heading">
-
-                                                            <p key={index}>{ele.head}</p>
-                                                        </div>
-                                                    </>
-
-                                                )
-                                            })}
+                                                    )
+                                                })}
+                                            </ol>
                                         </div>
-                                        <div className="col-6">
-                                            {Category.map((ele, index) => {
-                                                return (
-                                                    <>
-                                                        <div className="col-12  fontStyle footer_font_size footer_heading">
-
-                                                            <p>{ele.head2}</p>
-                                                        </div>
-                                                        <div className="col-12 Footer_fontStyle footer_font_size footer_heading">
-                                                            <p key={index}>{ele.head}</p>
-
+                                        <div className="col-6 footer_list">
+                                            <ol className="footer_list_gap">
+                                                <h5 className="fontStyle">Category</h5>
+                                                {Category.map((ele, index) => {
+                                                    return (
+                                                        <div key="index">
+                                                          <Link to="/"> <li>{ele.head}</li></Link>
                                                         </div>
 
-                                                    </>
 
-                                                )
-                                            })}
+                                                    )
+                                                })}
+                                            </ol>
+
                                         </div>
 
                                     </div>
@@ -82,42 +77,41 @@ const Footer = () => {
                                 </div>
                                 <div className="col-6 footer_content_width_small">
                                     <div className="col-12  footer_inner_container">
-                                        <div className="col-6">
-                                            {Legal.map((ele, index) => {
-                                                return (
-                                                    <>
-                                                        <div className="col-12 footer_font_size fontStyle footer_heading">
-                                                            <p>{ele.head3}</p>
+                                        <div className="col-6 footer_list">
+                                            <ol className="footer_list_gap ">
+                                                <h5 className="fontStyle">Legal</h5>
+
+                                                {Legal.map((ele, index) => {
+                                                    return (
+                                                        <div key="index">
+                                                            <Link to="/"> <li>{ele.head}</li></Link>
+
 
                                                         </div>
-                                                        <div className="col-12 Footer_fontStyle footer_font_size footer_heading">
-                                                            <p key={index}>{ele.head}</p>
 
-                                                        </div>
 
-                                                    </>
+                                                    )
 
-                                                )
-
-                                            })}
+                                                })}
+                                            </ol>
                                         </div>
-                                        <div className="col-6">
-                                            {More.map((ele, index) => {
-                                                return (
-                                                    <>
-                                                        <div className="col-12 footer_font_size fontStyle footer_heading">
-                                                            <p>{ele.head4}</p>
+                                        <div className="col-6 footer_list">
+                                            <ol className="footer_list_gap">
 
-                                                        </div>
-                                                        <div className="col-12 Footer_fontStyle footer_font_size footer_heading">
-                                                            <p key={index}>{ele.head}</p>
+                                                <h5 className="fontStyle">More</h5>
+                                                {More.map((ele, index) => {
+                                                    return (
+                                                        <>
+                                                            <div key="index">
+                                                            <Link to="/"> <li key="index">{ele.head}</li></Link>
 
-                                                        </div>
+                                                            </div>
 
-                                                    </>
-                                                )
+                                                        </>
+                                                    )
 
-                                            })}
+                                                })}
+                                            </ol>
                                         </div>
 
                                     </div>
