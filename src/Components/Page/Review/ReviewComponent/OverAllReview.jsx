e1c9ce2dc610d779e15d7913dcab0db31cc7b316
Chandrajeet123 <selnoxreact@gmail.com>
2023-04-22 02:47:43 +0530

    overallresult

--- src/Components/Page/Review/ReviewComponent/OverAllReview.jsx
+++ src/Components/Page/Review/ReviewComponent/OverAllReview.jsx
@@ -3,12 +3,12 @@ import { AiFillStar } from "react-icons/ai"
 const OverAllReview = () => {
 
     const testData = [
-        { bgcolor: "#6a1b9a", completed: 60 },
-        { bgcolor: "#00695c", completed: 30 },
-        { bgcolor: "#ef6c00", completed: 53 },
-        { bgcolor: "#ef6c00", completed: 53 },
+        { bgcolor: "#31B665", completed: 60 },
+        { bgcolor: "#31B665", completed: 30 },
+        { bgcolor: "#31B665", completed: 53 },
+        { bgcolor: "#31B665", completed: 53 },
 
-        { bgcolor: "#ef6c00", completed: 53 },
+        { bgcolor: "#31B665", completed: 53 },
 
     ];
     const starArray = [
