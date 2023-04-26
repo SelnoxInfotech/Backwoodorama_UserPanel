4cfc6abcde3d4caa29e56d862cbe12945b6e0bfd
Chandrajeet123 <selnoxreact@gmail.com>
2023-04-26 18:02:55 +0530

    hshd

---
+++ src/Components/Page/Product/EmptyCard/EmptyCard.jsx
@@ -0,0 +1,19 @@
+const EmptyCard=()=>{
+    return(
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12">
+                    <div className="image_container">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default EmptyCard
