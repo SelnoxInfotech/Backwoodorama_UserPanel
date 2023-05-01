9eda1651ee9dbab3317d53c4de5f6424122c1197
Chandrajeet123 <selnoxreact@gmail.com>
2023-05-01 17:49:27 +0530

    newproductcategory

--- src/Components/Page/Product/NewProductCategorySlider.jsx
+++ src/Components/Page/Product/NewProductCategorySlider.jsx
@@ -1,7 +1,7 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const NewProductCategorySlider = () => {
-    const flowerArray = [{ imgUrl: "/image/flower2.webp", title: "flower" }, { imgUrl: "/image/flower2.webp", title: "pre-roll" }, { imgUrl: "/image/flower2.webp", title: "flower" },
+    const flowerArray = [{ imgUrl: "/image/wee_img1.jpeg", title: "flower" }, { imgUrl: "/image/flower2.webp", title: "pre-roll" }, { imgUrl: "/image/flower2.webp", title: "flower" },
     { imgUrl: "/image/flower2.webp" }, { imgUrl: "/image/flower2.webp" },
     { imgUrl: "/image/flower2.webp", title: "flower" }, { imgUrl: "/image/flower2.webp", title: "flower" }, { imgUrl: "/image/flower2.webp", title: "flower" }]
     return (
@@ -12,11 +12,13 @@ const NewProductCategorySlider = () => {
                         {flowerArray.map((items, index) => {
                             return (
                                 <div className="row newProductCard " key={index}>
-                                    <div className="col-12 mx-4" >
-                                        <LazyLoadImage src={items.imgUrl} height={"100px"} />
-
+                                    <div className="col-12 mx-4">
+                                        <div className="newProduct_category_slider_image">
+                                        
+                                        <LazyLoadImage className="newProductCategory_image" src={items.imgUrl} />
+                                       </div>
                                     </div>
-                                    <div className="col-12" style={{marginLeft:"50px"}}>
+                                    <div className="col-12" style={{ marginLeft: "50px" }}>
                                         <p>flower</p>
                                     </div>
                                 </div>
