7b3c26a3cdb26d95fa69d8257ad047d971ac935d
Chandrajeet123 <selnoxreact@gmail.com>
2023-05-04 18:00:45 +0530

    prod

--- src/Components/Page/Product/ProductSearchResult/ProductIncDecQuantity.jsx
+++ src/Components/Page/Product/ProductSearchResult/ProductIncDecQuantity.jsx
@@ -32,17 +32,24 @@ const ProductIncDecQuantity = () => {
                 <Box sx={style}>
                     <div className='container-fluid'>
                         <div className='row'>
-                            <div className='col-lg-6'>
+                            <div className='col-6'>
                                 <LazyLoadImage src="/image/cat_pro_7.jpg" alt="image not available" height={"100px"}/>
                             </div>
-                            <div className='col-lg-6'>
+                            <div className='col-6'>
                                 <div className='row'>
-                                    <div className='col-12'>
+                                    <div className='col-12 prod_inc_dec_quant_heading'>
                                     <h1>Uraban flavour</h1>
 
                                     </div>
-                                    <div className='col-12'>
+                                    <div className='col-12 prod_inc_dec_quant_heading'>
                                     <h1>by Tribe Tokes</h1>
+
+                                    </div>
+                                    <div className='col-12 product_inc_dec_quantity_btn_cont'>
+                                        <button className='product_inc_dec_quantity_btn'>15% THC</button>
+                                        <button className='product_inc_dec_quantity_btn'>0.2% CBD</button>
+                                        <button className='product_inc_dec_quantity_btn'>Indica</button>
+
 
                                     </div>
 
