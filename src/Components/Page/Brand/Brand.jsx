5d01975c5df84d67a6b469f6fa70536308c50761
Chandrajeet123 <selnoxreact@gmail.com>
2023-05-05 23:50:06 +0530

    brands

--- src/Components/Page/Brand/Brand.jsx
+++ src/Components/Page/Brand/Brand.jsx
@@ -1,11 +1,12 @@
+import VerifyBrands from "./BrandComponent/VerifyBrands"
 const Brand=()=>{
     return(
         <>
         <div className="container-fluid">
-            <div className="row">
-                <div className="col-12">
+            <div className="row center">
+                <div className="col-10">
                     <h1>Verify Barnds</h1>
-
+                    <VerifyBrands/>
                 </div>
 
             </div>
--- src/Components/Page/Brand/BrandComponent/VerifyBrands.jsx
+++ src/Components/Page/Brand/BrandComponent/VerifyBrands.jsx
@@ -1,10 +1,32 @@
-const VerifyBrands=()=>{
-    const VerifyArrayData=[{imgUrl:"/image/flower.png",name:"All American"}]
-    return(
-        <>
-        <div className="row">
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
-        </div>
+const VerifyBrands = () => {
+    const VerifyArrayData = [{ imgUrl: "/image/flower.png", name: "All American" },
+    { imgUrl: "/image/glass.png", name: "All American" },
+    { imgUrl: "/image/flower.png", name: "All American" },
+    { imgUrl: "/image/glass.png", name: "All American" },
+    { imgUrl: "/image/flower.png", name: "All American" },
+    { imgUrl: "/image/glass.png", name: "All American" },            
+]
+    return (
+        <>
+            <div className="row">
+                {VerifyArrayData.map((items, index) => {
+                    return (
+                        <div className="col-md-4 verify_brand_container">
+                            <div className="row verifyBrand_row mx-1 my-1">
+                                <div className="col-4  verifyBrand_image_container">
+                                <LazyLoadImage className="product_search_result_image" src={items.imgUrl} alt="image not found"/>
+
+                                </div>
+                                <div className="col-8">
+                                    <p>{items.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
