19ca36df10f6ca8b8d09ad419f54410d7520b312
Chandrajeet123 <selnoxreact@gmail.com>
2023-05-05 16:31:21 +0530

    productIncDecQuantity

--- src/Components/Page/Product/ProductSearchResult/ProductIncDecQuantity.jsx
+++ src/Components/Page/Product/ProductSearchResult/ProductIncDecQuantity.jsx
@@ -1,34 +1,32 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { IoMdStar } from "react-icons/io";
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { GrFormSubtract } from "react-icons/gr"
+import { MdAdd } from "react-icons/md"
+import LoadingButton from '@mui/lab/LoadingButton';
+import useStyles from '../../../../Style';
+
+
 const style = {
     position: 'absolute',
-    top: '30%',
+    top: '40%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    "@media(min-width:800px)": {
-        width: "50%",
-        top: '30%',
 
-
-    },
     "@media(max-width:800px)": {
-        width: "90%",
-        top: '30%',
+        width: "80%",
+        marginTop: '10%',
 
     },
     "@media(max-width:500px)": {
-        width: "95%",
-        top: '30%',
+        width: "98%",
+        marginTop: '30%',
 
     },
 
@@ -41,11 +39,12 @@ const ProductIncDecQuantity = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [age, setAge] = React.useState('');
+    const [Data, SetData] = React.useState('');
 
     const handleChange = (event) => {
-        setAge(event.target.value);
+        SetData(event.target.value);
     };
+    const classes=useStyles()
     return (
         <>
             <Button onClick={handleOpen}>Buy now</Button>
@@ -58,37 +57,37 @@ const ProductIncDecQuantity = () => {
                 <Box sx={style}>
                     <div className='container-fluid'>
                         <div className='row'>
-                            <div className='col-6 productInc_dec_image_cont'>
-                                <LazyLoadImage className='prod_inc_dec_image' src="/image/cat_pro_7.jpg" alt="image not available" height={"100px"} />
+                            <div className='col-12 col-md-4  col-sm-6 productInc_dec_image_cont'>
+                                <LazyLoadImage className='prod_inc_dec_image' src="/image/cat_pro_7.jpg" alt="image not available"/>
                             </div>
-                            <div className='col-6 productInc_dec_content'>
+                            <div className='col-12 col-md-8 col-sm-6 productInc_dec_content'>
                                 <div className='row'>
                                     <div className='col-12 prod_inc_dec_quant_heading'>
-                                        <h1>Uraban flavour flavour</h1>
+                                        <h1>Uraban flavour Delivery</h1>
 
                                     </div>
                                     <div className='col-12 prod_inc_dec_quant_heading'>
-                                        <h1>by Tribe Tokes</h1>
+                                        <h2>by Tribe Tokes</h2>
 
                                     </div>
                                     <div className='col-12 product_inc_dec_quantity_btn_cont'>
                                         <button className='product_inc_dec_quantity_btn'>15% THC</button>
                                         <button className='product_inc_dec_quantity_btn'>0.2% CBD</button>
-                                        <button className='product_inc_dec_quantity_btn'>Indica</button>
+                                        <button className='product_inc_dec_quantity_btn_indica'>Indica</button>
 
 
                                     </div>
                                     <div className='col-12 d-flex gap-2'>
-                                        <span><IoMdStar /></span><span>4.5</span><span>Rating</span>
+                                        <span><IoMdStar className={classes.disp_star_color} /></span><span className='prod_qunat_rating'>4.5</span><span className='prod_qunat_rating'>Rating</span>
 
                                     </div>
-                                    <div className='col-2'>
+                                    <div className='col-lg-2 col-md-2 col-3 weight_font'>
                                         <p>weight</p>
                                     </div>
-                                    <div className='col-10'>
-                                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                                    <div className='col-lg-10 col-md-10 col-9 px-4'>
+                                        <FormControl sx={{ m: 1, minWidth: 120,background:"#F2F2F2" }} size="small">
                                             <Select
-                                                value={age}
+                                                value={Data}
                                                 onChange={handleChange}
                                                 displayEmpty
                                                 inputProps={{ 'aria-label': 'Without label' }}
@@ -96,12 +95,48 @@ const ProductIncDecQuantity = () => {
                                                 <MenuItem value="">
                                                     select weight
                                                 </MenuItem>
-                                                <MenuItem value={10}>Ten</MenuItem>
-                                                <MenuItem value={20}>Twenty</MenuItem>
-                                                <MenuItem value={30}>Thirty</MenuItem>
+                                                <MenuItem value={"1gm"}>1gm</MenuItem>
+                                                <MenuItem value={"2gm"}>2gm</MenuItem>
                                             </Select>
                                         </FormControl>
                                     </div>
+                                    <div className='col-lg-2 col-md-3 col-3 prod_quant_num'>
+
+                                        <p>Quantity</p>
+                                    </div>
+                                    <div className='col-lg-10 col-md-9 col-9   px-4'>
+                                        <div className='prod_inc_plus_minus mx-2 d-flex'>
+                                            <div className='Prod_subtract_div '>
+                                                <LoadingButton  className='subBtn1'>
+                                                    <GrFormSubtract />
+
+                                                </LoadingButton>
+                                            </div>
+                                            <div className='prod_digit'>
+                                                1
+                                            </div>
+                                            <div className='prod_addition_div'>
+                                                <LoadingButton  className='aaBtn2'>
+                                                    <MdAdd />
+
+                                                </LoadingButton>
+                                            </div>
+                                            <div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='col-12'>
+                                      <span className='prod_price_font'>$80.00</span> <span className='mx-4 prod_quant_font'>PER 1Z</span>
+                                    </div>
+
+                                    <div className='col-12 my-2'>
+                                <Box
+                                    className={` boxWidth ${classes.loadingBtnTextAndBack}`}
+                                >
+                                    <LoadingButton style={{width:"50%",height:"25px"}} variant="outlined" >Buy now</LoadingButton>
+                                </Box>
+                            </div>
 
                                 </div>
                             </div>
