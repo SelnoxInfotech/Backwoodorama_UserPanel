46351050ec52c505db888eace8b3ee1a4059ca44
Chandrajeet123 <selnoxreact@gmail.com>
2023-05-09 17:28:06 +0530

    wty

--- src/Components/Page/Product/NewProductDetails/NewProductDetails.jsx
+++ src/Components/Page/Product/NewProductDetails/NewProductDetails.jsx
@@ -61,14 +61,7 @@ const NewProductDetails = () => {
 
                                             )
                                         })}
-                                        {/* <SwiperSlide>Slide 2</SwiperSlide>
-                                        <SwiperSlide>Slide 3</SwiperSlide>
-                                        <SwiperSlide>Slide 4</SwiperSlide>
-                                        <SwiperSlide>Slide 5</SwiperSlide>
-                                        <SwiperSlide>Slide 6</SwiperSlide>
-                                        <SwiperSlide>Slide 7</SwiperSlide>
-                                        <SwiperSlide>Slide 8</SwiperSlide>
-                                        <SwiperSlide>Slide 9</SwiperSlide> */}
+                                      
                                     </Swiper>
                                 </div>
 
