845239713e3bfdc8a96736db38e687dcda881808
Selnoxinfotech <selnox93@gmail.com>
2023-05-18 15:07:12 +0530

    chmagejbj

--- src/Components/Page/Dispansires/DispansiresComponent/DispensoriesAddress.jsx
+++ src/Components/Page/Dispansires/DispansiresComponent/DispensoriesAddress.jsx
@@ -39,76 +39,76 @@ const DispensoriesAddress = () => {
 
             </div>
 
-            <div className='row'>
-                <div className="col-12   recentViewProductSlider" id="width" ref={ref}>
-                    <ScrollContainer className="ScrollContainerRelative">
 
-                        {Store.map((ele, index) => {
-                            return (
+            <div className="col-12   recentViewProductSlider" id="width" ref={ref}>
+                <ScrollContainer className="ScrollContainerRelative">
 
-                                <div className='col-lg-4 col-md-6 col-sm-6 col-12 dispensoriesContainer px-2' key={index} >
-                                    <div className='col-12 dispensories_image_containerss'>
-                                        <Link to={`/DispensoriesProduct/${ele.id}/${"Menu"}`}>
-                                            <img src={`http://backend.sweede.net/${ele?.Store_Image}`} alt='img_not_found' className='img-responsive dispensories_image rounded center-block'  />
-                                        </Link>
+                    {Store.map((ele, index) => {
+                        return (
+
+                            <div className='col-lg-4 col-md-6 col-sm-6 col-12 dispensoriesContainer px-2' key={index} >
+                                <div className='col-12 dispensories_image_containerss'>
+                                    <Link to={`/DispensoriesProduct/${ele.id}/${"Menu"}`}>
+                                        <img src={`http://backend.sweede.net/${ele?.Store_Image}`} alt='img_not_found' className='img-responsive dispensories_image rounded center-block' />
+                                    </Link>
+                                </div>
+                                <div>
+                                    <div className='col-12  '>
+
+                                        <div className=' col-10  dis_right_div'>
+                                            <p className='ellipsis'>{ele.Store_Name.charAt(0).toUpperCase() + ele.Store_Name.slice(1)}</p>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <div className='col-12  '>
+                                    <div className='col-12  '>
 
-                                            <div className=' col-10  dis_right_div'>
-                                                <p className='ellipsis'>{ele.Store_Name.charAt(0).toUpperCase() + ele.Store_Name.slice(1)}</p>
+                                        <div className=' col-10 Dispensaries_card_discription'>
+                                            <div className='col-2'>
+                                                <span className='span_nav'><BiMap className={classes.disPen_Icons} /></span>
+                                            </div>
+
+                                            <div className='col-10'>
+                                                <span className='ellipsis'>{ele.city_name}/110 mi</span>
                                             </div>
                                         </div>
-                                        <div className='col-12  '>
+                                    </div>
 
-                                            <div className=' col-10 Dispensaries_card_discription'>
-                                                <div className='col-2'>
-                                                    <span className='span_nav'><BiMap className={classes.disPen_Icons} /></span>
-                                                </div>
 
-                                                <div className='col-10'>
-                                                    <span className='ellipsis'>{ele.city_name}/110 mi</span>
-                                                </div>
+                                    <div className='col-12  '>
+                                        <div className=' col-10 Dispensaries_card_discription'>
+                                            <span className='ellipsis'>{ele.Store_Type}</span>
+                                        </div>
+                                    </div>
+                                    <div className='col-12  '>
+
+                                        <div className=' col-10 Rating'>
+                                            <div className='col-2 color'>
+                                                <span>Rating</span>
+                                            </div>
+
+                                            <div className='col-10'>
+                                                <span> <Rating name="read-only" value={4} readOnly /></span>
                                             </div>
                                         </div>
+                                    </div>
 
+                                    <div className='col-12   mt-4'>
 
-                                        <div className='col-12  '>
-                                            <div className=' col-10 Dispensaries_card_discription'>
-                                                <span className='ellipsis'>{ele.Store_Type}</span>
-                                            </div>
-                                        </div>
-                                        <div className='col-12  '>
-
-                                            <div className=' col-10 Rating'>
-                                                <div className='col-2 color'>
-                                                    <span>Rating</span>
-                                                </div>
-
-                                                <div className='col-10'>
-                                                    <span> <Rating name="read-only" value={4} readOnly /></span>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div className='col-12   mt-4'>
-
-                                            <div className=' col-10  mb-2'>
-                                                <Box
-                                                    className={`${classes.loadingBtnTextAndBack}`}
-                                                >
-                                                    <LoadingButton style={{width:"100%"}}>Order Pickup</LoadingButton>
-                                                </Box>
-                                            </div>
+                                        <div className=' col-10  mb-2'>
+                                            <Box
+                                                className={`${classes.loadingBtnTextAndBack}`}
+                                            >
+                                                <LoadingButton style={{ width: "100%" }}>Order Pickup</LoadingButton>
+                                            </Box>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
 
-                            )
-                        })}
-                    </ScrollContainer>
-                </div>
+                        )
+                    })}
+                </ScrollContainer>
             </div>
+
 
 
         </>
--- src/Components/Page/Home/Dashboard/Dashboard.jsx
+++ src/Components/Page/Home/Dashboard/Dashboard.jsx
@@ -37,7 +37,7 @@ export default function Dashboard() {
             <LatestServices></LatestServices>
             
             <CommunityType></CommunityType> 
-            <Footer></Footer>
+            {/* <Footer></Footer> */}
         </div>
     )
 }
