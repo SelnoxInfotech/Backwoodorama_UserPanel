c02353f711d3435bcf21d62b9f2561f42d4a6c5e
Chandrajeet123 <selnoxreact@gmail.com>
2023-05-20 00:13:13 +0530

    recentViewProduct

--- src/Components/Page/Product/RecentViewProduct/RecentViewProduct.jsx
+++ src/Components/Page/Product/RecentViewProduct/RecentViewProduct.jsx
@@ -1,6 +1,6 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { MdPlace } from "react-icons/md"
-import {GoStar} from "react-icons/go"
+import { GoStar } from "react-icons/go"
 import useStyles from "../../../../Style"
 import LoadingButton from '@mui/lab/LoadingButton';
 import Box from '@mui/material/Box';
@@ -39,58 +39,59 @@ const RecentViewProduct = () => {
 
 
     ]
- 
+
     return (
-       
+
         <div className="container-fluid" >
             <div className="row">
-                <div className="col-12   recentViewProductSlider"    id="width"ref={ref}>
-            <ScrollContainer className="ScrollContainerRelative">
-                    {FlowerArray.map((items, index) => {
-                        return (
-                        
-                            <div className="recentViewProductCard "   key={index} >
-                                <div className="row  mx-1">
-                                    <div className="col-12 center recentViewImageContainer p-2">
-                                        <LazyLoadImage className="recentView_images" src={items.imgUrl} alt="image not availble" />
-                                    </div>
-                                    <div className="col-12 recentViewProductContent_container mx-1">
+                <div className="col-12   recentViewProductSlider" id="width" ref={ref}>
+                    <ScrollContainer className="ScrollContainerRelative">
+                        {FlowerArray.map((items, index) => {
+                            return (
 
-                                        <div className="row">
-                                            <div className="col-12 recentViewProduct_heading">
-                                                <h1>Urban flavour delivery</h1>
+                            <div className='recentViewContainer col-12  col-sm-6 col-md-6 col-lg-4 col-xl-3' key={index}>
 
-                                            </div>
-                                            <div className="col-12">
-                                                <p><span><MdPlace className={`${classes.homePage_iconsColor}`}/></span> <span className="recentView_place">Berkeley California</span></p>
+                                <div className='recentViewProductBorder mx-1'>
 
-                                            </div>
-                                            <div className="col-12">
-                                                <p><span><GoStar className={`${classes.disp_star_color}`}/></span><span className="mx-1 recentView_rating">4.5 Rating</span></p>
-                                            </div>
-                                            <div className="col-12">
-                                            <Box
-                                            className={` weed_cart_btn ${classes.loadingBtnTextAndBack}`}
-                                            style={{ width: "100%" }}
-                                        >
+                                        <div className="col-12 center recentViewImageContainer p-2">
+                                            <LazyLoadImage className="recentView_images" src={items.imgUrl} alt="image not availble" />
+                                        </div>
+                                        <div className="col-12 recentViewProductContent_container px-1">
+
+                                                <div className="w-100 recentViewProduct_heading">
+                                                    <h1>Urban flavour delivery</h1>
+
+                                                </div>
+                                                <div className="w-100 recentViewProductParagraph">
+                                                    <p><span><MdPlace className={`${classes.homePage_iconsColor}`} /></span> <span className="recentView_place">Berkeley California</span></p>
+
+                                                </div>
+                                                
+                                                <div className="w-100 recentViewProductParagraph">
+                                                    <p><span><GoStar className={`${classes.disp_star_color}`} /></span><span className="mx-1 recentView_rating">4.5 Rating</span></p>
+                                                </div>
+                                                <div className="w-100 recentViewProduct_BtnContainer mt-4">
+                                                    <Box
+                                                        className={`mt-1  ${classes.loadingBtnTextAndBack}`}
+                                                        style={{ width: "100%" }}
+                                                    >
 
 
-                                           
-                                            <LoadingButton   style={{ width: "90%" }} variant="outlined"> Order Now</LoadingButton>
-                                        </Box>
+
+                                                        <LoadingButton style={{ width: "100%" }} variant="outlined"> Order Now</LoadingButton>
+                                                    </Box>
                                                 </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
-                </ScrollContainer>
+                            )
+                        })}
+                    </ScrollContainer>
                 </div>
             </div>
-          
+
         </div>
-                 
+
     )
 }
 export default RecentViewProduct
