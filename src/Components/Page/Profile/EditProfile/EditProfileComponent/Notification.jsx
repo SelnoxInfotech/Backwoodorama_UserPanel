c68f85f4a0faea6652c1bded1f0317246f28d6bb
Chandrajeet123 <selnoxreact@gmail.com>
2023-06-20 00:51:26 +0530

    Notification

--- src/Components/Page/Profile/EditProfile/EditProfileComponent/Notification.jsx
+++ src/Components/Page/Profile/EditProfile/EditProfileComponent/Notification.jsx
@@ -8,6 +8,9 @@ const Notification = () => {
     const EmailNotification = [{ heading: "News Letter", subHeading: "Weekly newsletter on the hottest topics and trends in the cannabis community." },
     { heading: "Review Suggestions", subHeading: "Reminders for you to share your experiences about the products and businesses you found on backaroma" }
     ]
+    const OrderUpdates = [{ heading: "Order Updates - Push Notifications", subHeading: "Receive timely updates to track the status of your order" },
+    { heading: "Order Updates - SMS Notifications", subHeading: "Receive timely updates to track the status of your order" }
+    ]
 
     return (
         <div className="col-12  mt-4">
@@ -23,7 +26,7 @@ const Notification = () => {
 
                         </div>
                         <div className="col-2 notification_col_ReactSwitch">
-                            <ReactSwitch checked={Checked} onChange={handleChange} />
+                            <ReactSwitch onColor={"#D9D9D9"}  onHandleColor={"#31B665"} height={20} width={60} uncheckedIcon={false} checkedIcon={false} checked={Checked} onChange={handleChange} />
                         </div>
 
 
@@ -43,7 +46,7 @@ const Notification = () => {
 
                                     </div>
                                     <div className="col-2 notification_col_ReactSwitch">
-                                        <ReactSwitch checked={Checked} onChange={handleChange} />
+                                        <ReactSwitch onColor={"#D9D9D9"}  onHandleColor={"#31B665"} height={20} width={60} uncheckedIcon={false} checkedIcon={false} checked={Checked} onChange={handleChange} />
                                     </div>
 
 
@@ -54,8 +57,77 @@ const Notification = () => {
                     }
 
                     <div className="row">
+                        <div className="col-10 notification_col_height2">
+
+                            <section className="notification_content_section">
+                                <span className="email_notification_headings">Push Notification</span>
+
+                            </section>
+
+                        </div>
+                        <div className="col-2 notification_col_ReactSwitch">
+                            <ReactSwitch onColor={"#D9D9D9"}  onHandleColor={"#31B665"} height={20} width={60} uncheckedIcon={false} checkedIcon={false} checked={Checked} onChange={handleChange} />
+                        </div>
+
+
 
                     </div>
+                    <div className="row">
+                        <div className="col-10 notification_col_height2">
+
+                            <section className="notification_content_section">
+                                <span className="email_notification_inner_headings">Recommendations</span>
+                                <p className="email_notification_sub_headings">Recommendations of products we think you'll love.</p>
+
+                            </section>
+
+                        </div>
+                        <div className="col-2 notification_col_ReactSwitch">
+                            <ReactSwitch onColor={"#D9D9D9"}  onHandleColor={"#31B665"} height={20} width={60} uncheckedIcon={false} checkedIcon={false} checked={Checked} onChange={handleChange} />
+                        </div>
+
+
+
+                    </div>
+                    <div className="row">
+                        <div className="col-10 notification_col_height2">
+
+                            <section className="notification_content_section">
+                                <span className="email_notification_headings">Savings</span>
+                                <p className="email_saving_sub_heading">Daily updates about savings nearby.</p>
+
+                            </section>
+
+                        </div>
+                        <div className="col-2 notification_col_ReactSwitch">
+                            <ReactSwitch onColor={"#D9D9D9"}  onHandleColor={"#31B665"} height={20} width={60} uncheckedIcon={false} checkedIcon={false} checked={Checked} onChange={handleChange} />
+                        </div>
+
+
+
+                    </div>
+                    {
+                        OrderUpdates.map((items,index)=>{
+                            return(
+                                <div className="row" key={index}>
+                                    <div className="col-10 notification_col_height2">
+
+                                        <section className="notification_content_section">
+                                            <span className="email_notification_inner_headings">{items.heading}</span>
+                                            <p className="email_notification_sub_headings">{items.subHeading}</p>
+                                        </section>
+
+                                    </div>
+                                    <div className="col-2 notification_col_ReactSwitch">
+                                        <ReactSwitch onColor={"#D9D9D9"}  onHandleColor={"#31B665"} height={20} width={60} uncheckedIcon={false} checkedIcon={false} checked={Checked} onChange={handleChange} />
+                                    </div>
+
+
+
+                                </div>
+                            )
+                        })
+                    }
 
 
                 </div>
