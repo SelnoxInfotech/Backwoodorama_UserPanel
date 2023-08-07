7bed95d9bc77afa76e1d87faaf356c9a1d6df191
Chandrajeet123 <selnoxreact@gmail.com>
2023-08-07 22:24:40 +0530

    whislist

--- src/Components/Component/Whishlist/WhisLists/WhisListComponent/WhisListCards.jsx
+++ src/Components/Component/Whishlist/WhisLists/WhisListComponent/WhisListCards.jsx
@@ -35,14 +35,14 @@ const WhisListCard = () => {
             {GetApiData.map((items, index) => {
                 return (
                     <React.Fragment key={index}>
-                        <div className="col_xl_width col-lg-4 col-md-5 col-sm-6 col-12" >
+                        <div className="col_xl_width col-lg-4 col-md-5 col-sm-5 col-12" >
                             <div className="col-12 whislistCard">
                                 <div className="col-12 whislist_iconsContainer">
                                     <IconButton><AiOutlineHeart /></IconButton>
                                 </div>
                                 <div className="col-12 whislistImgCenter">
                                     <div className="whislist_innerImgContainer">
-                                        <LazyLoadImage src="./image/cat_pro_7.jpg" className="whislist_imageStyle border" alt="imgs-not-found" onClick={()=>Navigate(`/NewProductDetails/${items.product}`)} />
+                                        <LazyLoadImage src="./image/cat_pro_7.jpg" className="whislist_imageStyle" alt="imgs-not-found" onClick={()=>Navigate(`/NewProductDetails/${items.product}`)} />
                                     </div>
                                 </div>
                                 <div className="col-12 whislistCard_Content_Container">
@@ -53,7 +53,7 @@ const WhisListCard = () => {
                                         <MdOutlinePlace color="#31B665" /><span className="whislistAddress">2917 Broadway Astoria, NY 11106</span>
                                     </div>
                                     <div className="WhishListCard_paddingLeft">
-                                        <p className="whislistAddress multine-ellipsis">Tyonek Alaska  | 110 mi errrrrrrrrrrrrrrrrrrrrrrrrrrrrr errrrrrrrrrrrrrr</p>
+                                        <p className="whislistAddress multine-ellipsis">Tyonek Alaska  | 110 mi</p>
                                     </div>
                                     <div className="spaceIcons_content_Container WhishListCard_paddingLeft">
                                         <span className="whislistAddress">Rating</span><Rating name="read-only" className={classes.homePageStarIcons} value={4} readOnly />
