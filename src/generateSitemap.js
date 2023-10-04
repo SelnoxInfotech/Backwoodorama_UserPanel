71a6bdca15c3df50474289073e010fd5ab687e14
swapnil <selnox93@gmail.com>
2023-10-04 23:48:48 +0530

    sitmap

--- src/generateSitemap.js
+++ src/generateSitemap.js
@@ -1,29 +1,37 @@
-
+// import React from 'react';
 var fs = require('fs');
-
-function generateSitemap() {
+var axios = require('axios')
+// import  Axios  from 'axios';
+async function generateSitemap() {
     // Fetch dynamic URLs from your e-commerce website's data source.
     // You may need to make API calls or use your database here.
-    const dynamicUrls = [
-      '/products/product-1',
-      '/products/product-2',
-      // Add more dynamic URLs here
-    ];
+    // React.useEffect(() => {
+      const fetchSitemapData = async () => {
+        try {
+          const response = await axios.get('https://api.cannabaze.com/UserPanel/Get-Categories/');
+          return response.data;
+        } catch (error) {
+          console.error('Error fetching sitemap data:', error);
+          return [];
+        }
+      };
+      console.log( fetchSitemapData)
   
     // Create the sitemap XML string
-    const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${dynamicUrls.map((url) => `
-          <url>
-            <loc>https://example.com${url}</loc>
-            <changefreq>daily</changefreq>
-            <priority>0.7</priority>
-          </url>
-        `).join('')}
-      </urlset>`;
+    // const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+    //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    //     ${Category.map((url) => `
+    //       <url>
+    //         <loc>https://example.com${url}</loc>
+    //         <changefreq>daily</changefreq>
+    //         <priority>0.7</priority>
+    //       </url>
+    //     `).join('')}
+    //   </urlset>`;
   
-    // Write the sitemap XML to a file
-    fs.writeFileSync('/public/sitemap.xml', sitemapXml);
+    // // Write the sitemap XML to a file
+    // fs.writeFileSync('../public/Sitemap/sitemap.xml', sitemapXml);
+    // fs.writeFileSync('../public/Sitemap/sitemap1.xml', sitemapXml);
   
     console.log('Sitemap generated and saved as sitemap.xml');
   }
