42265c94b9cef8848fe93e8c675fa454644bfd0a
swapnil chauhan <selnox93@gmail.com>
2023-10-30 10:39:38 +0530

    sd

--- src/Components/Component/Filter/ProductFilter.jsx
+++ src/Components/Component/Filter/ProductFilter.jsx
@@ -259,7 +259,6 @@ const ProductFilter = ({ ProductFilterData, arr, Setarr1, Store_id }) => {
         
        const timer = setTimeout(() => {
             PriceFilter(value).then((res) => {
-                console.log(res)
             }).catch(() => {
                 // navigate('/fourzerothree')   
             })
