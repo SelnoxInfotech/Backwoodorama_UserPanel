7068f647ea8101d89e272ad54a43687ef2fa194a
swapnil chauhan <selnox93@gmail.com>
2023-11-07 18:22:46 +0530

    vhnge

--- src/Components/Page/Brand/RelatedVerifyBrand/RelatedVerifyComponent/RelatedVerifyBrandBanner.jsx
+++ src/Components/Page/Brand/RelatedVerifyBrand/RelatedVerifyComponent/RelatedVerifyBrandBanner.jsx
@@ -4,59 +4,61 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import  IconButton  from "@mui/material/IconButton";
 import { RWebShare } from "react-web-share";
 import './RelatedVerifyBrandBanner.css';
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState ,useEffect } from "react";
+import React from "react"; 
+import { Helmet } from "react-helmet-async";
 const RelatedVerifyBanner = ({ BrandDetails }) => {
     const [readmore , setreadmore] = useState(false)
 
-  function my (r){
-    setreadmore(!readmore)
-  }
-   
+
     return (
-        <div className="brandProfileBanner row center">
-            <div className={"relatedVerifyBrand_Banner"}>
-              
-                    <div className="relatedVerifyBrand_icons">
-                       <IconButton  aria-label="share icons"><AiTwotoneHeart color="#31B665" size={20} /></IconButton>
-
-                        <IconButton  aria-label="share icons">
-                        <RWebShare
-                                        data={{ url: window.location.href }}
-                                        sites={["facebook", "twitter", "whatsapp", "telegram", "linkedin", 'mail', 'copy']}
-                                        onClick={() => console.info("share successful!")}
-                                        color="#31B665" >
-                                          <MdShare color="#949494" size={20} />
-                                    </RWebShare>
-                          </IconButton>
-
-                    </div>
-                    <div className="related_verifyBrandBanner_maincol">
-                        <div className="RelatedVerifyBrandBanner_image_box">
-                            <div className="realtedVerifyBanner_image_inner_container">
-                                <LazyLoadImage className="related_verify_banner_img"     src={ BrandDetails.Brand_Logo} alt="Brand_Logo image not available" />
-                            </div>
+     <React.Fragment>
+ 
+            <div className="brandProfileBanner row center">
+                
+                <div className={"relatedVerifyBrand_Banner"}>
+                  
+                        <div className="relatedVerifyBrand_icons">
+                           <IconButton  aria-label="share icons"><AiTwotoneHeart color="#31B665" size={20} /></IconButton>
+    
+                            <IconButton  aria-label="share icons">
+                            <RWebShare
+                                            data={{ url: window.location.href }}
+                                            sites={["facebook", "twitter", "whatsapp", "telegram", "linkedin", 'mail', 'copy']}
+                                            onClick={() => console.info("share successful!")}
+                                            color="#31B665" >
+                                              <MdShare color="#949494" size={20} />
+                                        </RWebShare>
+                              </IconButton>
+    
                         </div>
-                        <div className="RelatedVerifyBanner_content_box">
-                     
-                                <h1 className="section_main_title">{BrandDetails.name}</h1>
-                          
-                            <div className={readmore ? "related_verify_paragraph" : " brandMoreLess" }>
-
-                                <div 
-                                 dangerouslySetInnerHTML=
-                                 {{ __html: readmore ?  BrandDetails.Brand_description +
-                                  ` <spa id="band_shlebtn"  class="band_shlebtn" onclick='${my}' > Read less</spa>` :
-                                    BrandDetails.Brand_description.split('</p>')[0] + ` <span  onclick='${my}' class="band_shlebtn" >  Read More</span>` } }  />
-                               
+                        <div className="related_verifyBrandBanner_maincol">
+                            <div className="RelatedVerifyBrandBanner_image_box">
+                                <div className="realtedVerifyBanner_image_inner_container">
+                                    <LazyLoadImage className="related_verify_banner_img"     src={ BrandDetails.Brand_Logo} alt="Brand_Logo image not available" />
+                                </div>
                             </div>
-                          
-                        </div>
-                    </div>                
-              
-
+                            <div className="RelatedVerifyBanner_content_box">
+                         
+                                    <h1 className="section_main_title">{BrandDetails.name}</h1>
+                              
+                                <div className={readmore ? "related_verify_paragraph" : " brandMoreLess" }>
+    
+                                    <div 
+                                     dangerouslySetInnerHTML=
+                                     {{ __html: readmore ?  BrandDetails.Brand_description +
+                                      ` <span id="band_shlebtn"  class="band_shlebtn"  onclick='dosomething();' > Read less</span>` :
+                                        BrandDetails.Brand_description.split('</p>')[0] + `<span onclick='dosomething();' class="band_shlebtn" >  Read More</span>` } }  />
+                                   
+                                </div>
+                              
+                            </div>
+                        </div>                
+                  
+    
+                </div>
             </div>
-        </div>
+     </React.Fragment>
     )
 }
 export default RelatedVerifyBanner
