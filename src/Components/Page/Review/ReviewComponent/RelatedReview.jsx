dcb3dd145f8672e8d7eaa1ee4377cd49db5ae526
swapnil <selnox93@gmail.com>
2023-11-18 20:18:32 +0530

    chnage

--- src/Components/Page/Review/ReviewComponent/RelatedReview.jsx
+++ src/Components/Page/Review/ReviewComponent/RelatedReview.jsx
@@ -10,7 +10,7 @@ import ReportReviewPopup from '../ReviewPopup/ReportReviewPopup';
 import { useState } from 'react';
 import Createcontext from "../../../../Hooks/Context"
 import { Delete_Review } from "../../Product/ProductApi"
-const RelatedReview = ({ handleEdit,AllReview, handleDelete }) => {
+const RelatedReview = ({ handleEdit, AllReview, handleDelete }) => {
     const classes = useStyles()
     const { state, dispatch } = React.useContext(Createcontext)
     const [readopen, setreadopen] = useState(true)
@@ -86,94 +86,56 @@ const RelatedReview = ({ handleEdit,AllReview, handleDelete }) => {
                                         </div>
                                         <div className="related_review_content">
 
-                                            <h3 className='reviews_title'>{ele.Title}</h3>
-    <p>{ele.username}</p>
+                                        <h3 className='reviews_title'>{ele.Title}</h3>
+<p>{ele.username}</p>
 
-                                            <div className="reviwerName_rating">
+                                        <div className="reviwerName_rating">
 
-                                                <div className='reviewSectionRating'>
-                                                    {ele.rating && new Array(ele.rating).fill(null).map(() => (
-                                                        <BsStarFill size={10} color="#31B665" className="product_search_rating_star" />
-                                                    ))}
+                                            <div className='reviewSectionRating'>
+                                                {ele.rating && new Array(ele.rating).fill(null).map(() => (
+                                                    <BsStarFill size={10} color="#31B665" className="product_search_rating_star" />
+                                                ))}
 
-                                                    {new Array(5 - ele.rating).fill(null).map(() => (
-                                                        <BsStar size={10} color="#31B665" className="product_search_rating_star" />
-                                                    ))}
-                                                </div>
+                                                {new Array(5 - ele.rating).fill(null).map(() => (
+                                                    <BsStar size={10} color="#31B665" className="product_search_rating_star" />
+                                                ))}
                                             </div>
-                                            <div className='review_date'>
-                                                <p>{ele.created_at.slice(0, 10).split("-").reverse().join("-")}</p>
-                                                {
-                                                    state.login &&
-                                                    state.Profile.id === ele.user &&  <span>
-                                                        <IconButton
-                                                            onClick={handleoption}
+                                        </div>
+                                        <div className='review_date'>
+                                            <p>{ele.created_at.slice(0, 10).split("-").reverse().join("-")}</p>
+                                            {
+                                                state.login &&
+                                                state.Profile.id === ele.user &&  <span>
+                                                    <IconButton
+                                                        onClick={handleoption}
 
                                                         >
                                                             <BsThreeDotsVertical size={10}></BsThreeDotsVertical>
 
-                                                        </IconButton>
-                                                        {Option && <>
-                                                        <option onClick={()=>handleDelete(ele.id)} >Delete</option>
-                                                        <option onClick={()=>handleEdit()} >Edit</option>
-                                                        </> }
-                                                    </span>
-                                                }
-                                            </div>
+                                                    </IconButton>
+                                                    {Option && <>
+                                                    <option onClick={()=>handleDelete(ele.id)} >Delete</option>
+                                                    <option onClick={()=>handleEdit()} >Edit</option>
+                                                    </> }
+                                                </span>
+                                            }
                                         </div>
-
-                                    </div>
-                                    <div className='review_description_container'>
-                                       
-                                        <p>{textgive(text)}   { text.split(' ').length >= 100 &&<span className='band_shlebtn' onClick={()=>setreadopen(!readopen)}>Read { readopen ? "More" : "Less"}</span>}</p>
                                     </div>
 
-                                    <div className='container-fluid'>
-                                    <div className="d-flex gap-2">
-                                        <div className="related_img_container">
+                                </div>
+                                <div className='review_description_container'>
 
-                                            <div className="related_review_image">
+                                    <p>{textgive(text)}   {text.split(' ').length >= 100 && <span className='band_shlebtn' onClick={() => setreadopen(!readopen)}>Read {readopen ? "More" : "Less"}</span>}</p>
+                                </div>
+                                <div className='related_review_footer '>
 
-                                                <LazyLoadImage
-                                                    onError={event => {
-                                                        event.target.src = "/image/user.webp"
-                                                        event.onerror = null
-                                                    }}
-                                                    className='realted_review_images'
-                                                    src={`${ele?.userImage}`}
-                                                    alt="userImage"
-                                                />
-                                            </div>
-
-                                        </div>
-                                        <div className="related_review_content">
-
-                                            <h3 className='reviews_title'>{ele.Title}</h3>
-                                            <p>{ele.username}</p>
-
-                                            <div className="reviwerName_rating">
-
-                                              
-                                            </div>
-                                        
-                                        </div>
-
+                                    <div className='related_review_footer_paragraph ellipsis'>
+                                        <Button className={classes.WriteReviewBtn_Color} variant="outlined"> <AiFillLike /> Helpful</Button>
                                     </div>
-                                    <div className='review_description_container'>
-                                       
-                                        <p>{textgive(text)}   { text.split(' ').length >= 100 &&<span className='band_shlebtn' onClick={()=>setreadopen(!readopen)}>Read { readopen ? "More" : "Less"}</span>}</p>
-                                    </div>
+                                    <div className='related_review_footer_paragraph ellipsis px-0'>
 
+                                        <ReportReviewPopup />
                                     </div>
-                                    <div className='related_review_footer '>
-                                       
-                                            <div className='related_review_footer_paragraph ellipsis'>
-                                               <Button className={classes.WriteReviewBtn_Color} variant="outlined"> <AiFillLike/> Helpful</Button>
-                                            </div>
-                                            <div className='related_review_footer_paragraph ellipsis px-0'>
-                                              
-                                                <ReportReviewPopup/>
-                                            </div>
 
 
                                     </div>
