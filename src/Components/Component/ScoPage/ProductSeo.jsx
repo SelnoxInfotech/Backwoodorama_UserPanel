8e91e5e9a18c25ebed9d0890e00934ec56b62ce0
ashwaniyadav <selnoxreact@gmail.com>
2024-05-31 10:30:19 +0530

    meta description

--- src/Components/Component/ScoPage/ProductSeo.jsx
+++ src/Components/Component/ScoPage/ProductSeo.jsx
@@ -1,23 +1,23 @@
 import { Helmet } from 'react-helmet-async';
 import React from "react"
 import { useLocation } from "react-router-dom"
-function ProductDetailsSeo({Productname , ProductCategory , StoreName ,  City , State ,location}) {
+function ProductDetailsSeo({Productname ,Productnm, ProductCategory , StoreName ,  City , State ,location}) {
     return (
         <Helmet>
             <title> {`${Productname}  `}</title>
             <meta name="title" content={`${Productname}`}/>
-            <meta name='description' content={`${Productname} - ${ProductCategory} at ${StoreName} - Your Ultimate Cannabis ${useLocation().pathname.slice(0, 16) === "/weed-deliveries" ? `Delivery` : `Dispensary`} in ${City}, ${State}.`} />
+            <meta name='description' content={`${Productnm} - ${ProductCategory} at ${StoreName} - Your Ultimate Cannabis ${useLocation().pathname.slice(0, 16) === "/weed-deliveries" ? `Delivery` : `Dispensary`} in ${City}, ${State}.`} />
            <link rel="canonical" href={`https://www.weedx.io${location}`} /> 
             {/* Facebook tags */}
             <meta property="og:type" content="website"/>
             <meta property="og:title" content={`${Productname}`} />
-            <meta property="og:description" content={`${Productname} - ${ProductCategory} at ${StoreName} - Your Ultimate Cannabis ${useLocation().pathname.slice(0, 16) === "/weed-deliveries" ? `Delivery` : `Dispensary`} in ${City}, ${State}.`} />
+            <meta property="og:description" content={`${Productnm} - ${ProductCategory} at ${StoreName} - Your Ultimate Cannabis ${useLocation().pathname.slice(0, 16) === "/weed-deliveries" ? `Delivery` : `Dispensary`} in ${City}, ${State}.`} />
             { /* End Facebook tags */}
             { /* Twitter tags */}
             <meta name="twitter:creator" content={"website"} />
             <meta name="twitter:card" content={"website Dispensaries & `Delivery` Near Me"} />
             <meta name="twitter:title" content={`${Productname}`} />
-            <meta name="twitter:description" content={`${Productname} - ${ProductCategory} at ${StoreName} - Your Ultimate Cannabis ${useLocation().pathname.slice(0, 16) === "/weed-deliveries" ? `Delivery` : `Dispensary`} in ${City}, ${State}.`} />
+            <meta name="twitter:description" content={`${Productnm} - ${ProductCategory} at ${StoreName} - Your Ultimate Cannabis ${useLocation().pathname.slice(0, 16) === "/weed-deliveries" ? `Delivery` : `Dispensary`} in ${City}, ${State}.`} />
         </Helmet>
     )
 }
